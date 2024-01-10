55d0511f9a4c501fc8654435a9114b5991c06a44
Yahya Ergün <yahyaergun@Yahyas-MacBook-Pro.local>
2024-01-10 13:13:40 +0100

    ignore unknown

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,5 +1,6 @@
 package org.leanpoker.player;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,20 +10,20 @@ import org.leanpoker.player.service.PokerService;
 public class Player {
 
     static PokerService pokerService = new PokerService();
+
+    static ObjectMapper objectMapper = new ObjectMapper().configure(JsonGenerator.Feature.IGNORE_UNKNOWN, true);
     static final String VERSION = "Default Java folding player";
 
 
     public static int betRequest(JsonNode request) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
 
         System.out.println(request.toString());
         GameStatePOJO gameStatePOJO = objectMapper.treeToValue(request, GameStatePOJO.class);
-        return 0;
-        // Now you can work with the GameStatePOJO object
-//        System.out.println("Tournament ID: " + gameStatePOJO.getTournament_id());
-//        System.out.println("Game ID: " + gameStatePOJO.getGame_id());
 
-//        return pokerService.prepareBet(gameStatePOJO);
+        System.out.println("Tournament ID: " + gameStatePOJO.getTournament_id());
+        System.out.println("Game ID: " + gameStatePOJO.getGame_id());
+
+        return pokerService.prepareBet(gameStatePOJO);
     }
 
     public static void showdown(JsonNode game) throws JsonProcessingException {
